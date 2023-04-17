8649869791fd6ed027ba1814bc1a750f09091cc6
Filippe Braga <filippeffx@hotmail.com>
2023-04-17 19:34:02 -0300

    notecard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import Head from "next/head";
 import { useSession } from "next-auth/react";
 
 import { api, type RouterOutputs } from "@/utils/api";
-import { Header } from "../components/Header";
+import { Header } from "@/components/Header";
 import { useState } from "react";
 import { NoteEditor } from "@/components/NoteEditor";
+import { NoteCard } from "@/components/NoteCard";
 
 const Home: NextPage = () => {
   return (
@@ -61,7 +62,13 @@ const Content: React.FC = () => {
     onSuccess: () => {
       void refetchNotes();
     },
-  })
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetchNotes();
+    },
+  });
 
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
@@ -98,6 +105,17 @@ const Content: React.FC = () => {
       
       </div>
       <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard 
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+
         <NoteEditor 
           onSave={({ title, content }) => {
             void createNote.mutate({
